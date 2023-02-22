b50c3798648696bb653cc2d3ac54eeecbaa68f84
oBonn14 <exsanal1515@gmail.com>
2023-02-22 10:42:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function toggleDetail(e) {
+    const target = $(e.target)
+
+    if($(target).hasClass("active")) {
+        $(target).html("More Info").removeClass("active")
+    } else {
+        $(target).html("Less Info").addClass("active")
+    }
+    
+    const item = $(target).parents(".myproject-item")
+    const detail = $(item).children(".myproject-item-detail")
+    $(detail).slideToggle()
+}
